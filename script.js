5e29abefea9760bea517fce1fcea9c1e5d0b00c1
tonypowers09 <tpowers09@gmail.com>
2023-05-29 00:26:03 -0700

    ed

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-const title = document.getElementById('title');
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-let currentIndex = 0;
+document.addEventListener('DOMContentLoaded', function() {
+  const title = document.getElementById('title');
+  const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+  let currentIndex = 0;
 
-function changeColor() {
-  title.style.color = colors[currentIndex];
-  currentIndex = (currentIndex + 1) % colors.length;
-}
+  function changeColor() {
+    title.style.color = colors[currentIndex];
+    currentIndex = (currentIndex + 1) % colors.length;
+  }
 
-setInterval(changeColor, 1000);
-setInterval(changeColor, 17000);
+  setInterval(changeColor, 1000);
+  setInterval(changeColor, 17000);
+});
 
