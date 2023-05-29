ccb0ee609756067cc53d14f90460fcc3d4ef7791
tonypowers09 <tpowers09@gmail.com>
2023-05-29 00:42:02 -0700

    ed

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const title = document.getElementById('title');
 
 setInterval(() => {
-  const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-  title.style.color = randomColor;
-}, 1000);
+  const colors = ['#FF0000', '#FF4500', '#FFA500', '#FFFF00', '#00FF00', '#00FFFF', '#0000FF', '#8A2BE2', '#EE82EE'];
+  let currentIndex = 0;
+
+  const changeColor = () => {
+    title.style.color = colors[currentIndex];
+    currentIndex = (currentIndex + 1) % colors.length;
+  };
+
+  let intervalId = setInterval(changeColor, 1000);
+
+  setTimeout(() => {
+    clearInterval(intervalId);
+    title.style.color = ''; // Reset to default color
+  }, 17000);
+}, 17000);
