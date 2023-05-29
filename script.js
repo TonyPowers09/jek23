f9b6859bedab325fdd602038feeba9f7ea36bf8b
tonypowers09 <tpowers09@gmail.com>
2023-05-29 01:32:52 -0700

    ed

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ const title = document.getElementById('title');
 const colors = ['#FF0000', '#FF4500', '#FFA500', '#FFFF00', '#00FF00', '#00FFFF', '#0000FF', '#8A2BE2', '#EE82EE'];
 let currentIndex = 0;
 
-const handleClick = () => {
-  window.location.href = 'https://pdfz.s3.us-west-1.amazonaws.com/giphy.gif';
+const changeColor = () => {
+  title.style.color = colors[currentIndex];
+  currentIndex = (currentIndex + 1) % colors.length;
+};
+
+const handleClick = (event) => {
+  event.preventDefault();
+  window.open('https://pdfz.s3.us-west-1.amazonaws.com/giphy.gif', '_blank');
 };
 
 title.addEventListener('click', handleClick);
 setInterval(changeColor, 1000);
-
