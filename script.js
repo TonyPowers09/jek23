62c89d1145c2644b6255a5abc935b7244e5fda00
tonypowers09 <tpowers09@gmail.com>
2023-05-29 01:29:19 -0700

    ed

--- script.js
+++ script.js
@@ -2,11 +2,6 @@ const title = document.getElementById('title');
 const colors = ['#FF0000', '#FF4500', '#FFA500', '#FFFF00', '#00FF00', '#00FFFF', '#0000FF', '#8A2BE2', '#EE82EE'];
 let currentIndex = 0;
 
-const changeColor = () => {
-  title.style.color = colors[currentIndex];
-  currentIndex = (currentIndex + 1) % colors.length;
-};
-
 const handleClick = () => {
   window.location.href = 'https://pdfz.s3.us-west-1.amazonaws.com/giphy.gif';
 };
